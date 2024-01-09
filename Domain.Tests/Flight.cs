87f2c32ea48364987a616ab1161fdae8acb67155
SairaBeg <sairabeg.dev@gmail.com>
2024-01-08 22:18:10 -0500

    Flight class, test written

--- Domain.Tests/Flight.cs
+++ Domain.Tests/Flight.cs
@@ -1,7 +1,34 @@
 ﻿namespace Domain.Tests
+  
 {
     public class Flight
     {
+        public int seatCapacity {  get; set; }
+        public int passengers { get; set; }
+        public int RemainingNumberOfSeats { get; set; }
+        public Flight(int seatCapacity)
+        {
+            seatCapacity = this.seatCapacity;
+        }
+
+        public void Book(string v1,int newPassengers)
+        {
+            throw new NotImplementedException();
+            //if (passengers == seatCapacity)
+            //{
+            //    Console.WriteLine("The flight is completely booked");
+
+            //} else if (newPassengers+passengers > seatCapacity)
+            //{
+            //    Console.WriteLine("Booking that many passengers would overbook the flight.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Booking your flight...");
+            //    passengers += newPassengers;
+            //    Console.WriteLine($"Your flight is now booked. There are {passengers} passengers out of {seatCapacity} booked");
+            //}
+        }
 
     }
 }
--- Flight/UnitTest1.cs
+++ Flight/UnitTest1.cs
@@ -1,11 +1,20 @@
-namespace Flight
+using Domain.Tests;
+using Xunit;
+using FluentAssertions;
+
+namespace FlightTest
+
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
+            var flight = new Flight(seatCapacity: 3);
 
+            flight.Book("email", 1);
+
+           flight.RemainingNumberOfSeats.Should().Be(2);
         }
     }
 }
